4d1dec797dc9c578f3b59acd7c721d86adc4392f
tosuki <okaabe2006@gmail.com>
2025-03-06 19:33:23 -0300

    feat: add remove function to window collection

--- src/window_node.c
+++ src/window_node.c
@@ -56,6 +56,46 @@ WindowNode* get_windownode(WindowNode* tail, Window content_xid) {
     return get_windownode(tail->next, content_xid);
 }
 
+WindowNode* get_previous_windownode(WindowNode* tail, Window content_xid) {
+    if (tail == NULL || tail->next == NULL) {
+        return NULL;
+    }
+
+    if (tail->next->content == content_xid) {
+        return tail;
+    }
+
+    return get_previous_windownode(tail->next, content_xid);
+}
+
+WindowNode* remove_windownnode(WindowNode* tail, Window content_xid) {
+    if (tail == NULL) {
+        return NULL;
+    }
+
+    if (tail->content == content_xid) {
+        WindowNode* next = tail->next;
+        free(tail);
+        return next;
+    }
+    
+    WindowNode* previous_node = get_previous_windownode(tail, content_xid);
+    
+    if (previous_node == NULL) {
+        return tail;
+    }
+
+    WindowNode* next = previous_node->next->next;
+    free(previous_node->next);
+    previous_node->next = next;
+
+    return tail;
+}
+
+void window_collection_delete(Windows* collection, Window content_xid) {
+    collection->nodes = remove_windownnode(collection->nodes, content_xid);
+}
+
 WindowNode* window_collection_get(Windows* windows, Window content_xid) {
     return get_windownode(windows->nodes, content_xid);
 }
--- src/window_node.h
+++ src/window_node.h
@@ -16,6 +16,7 @@ typedef struct Windows {
 
 Windows* create_window_collection();
 void free_window_collection(Windows* windows);
+void window_collection_delete(Windows* collection, Window content_xid);
 WindowNode* window_collection_get(Windows* collection, Window content_xid);
 WindowNode* window_collection_add(Windows* collection, Window frame_xid, Window content_xid);
 WindowNode* create_windownode(Window frame, Window content, WindowNode* next);
